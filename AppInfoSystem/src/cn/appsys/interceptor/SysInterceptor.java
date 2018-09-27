e8edc0ecad916ab4df49ace8efa650f1c5f11c40
Administrator <Administrator@965P1BRVEEN2KWU>
2018-09-27 17:18:59 +0800

    初始化项目

---
+++ AppInfoSystem/src/cn/appsys/interceptor/SysInterceptor.java
@@ -0,0 +1,26 @@
+package cn.appsys.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+
+
+public class SysInterceptor extends HandlerInterceptorAdapter {
+	private Logger logger = Logger.getLogger(SysInterceptor.class);
+	
+	public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object handler) throws Exception{
+		logger.debug("SysInterceptor preHandle ==========================");
+		HttpSession session = request.getSession();
+		/*User user = (User)session.getAttribute(Constants.USER_SESSION);
+		if(user == null) {
+			response.sendRedirect(request.getContextPath()+"/403.jsp");
+			return false;
+		}*/
+		return true;
+	}
+}
